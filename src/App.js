5ca506fdfad23d628db781b7bc4397309a118e3d
Rayane Bitchi <rayamevoltaire@gmail.com>
2024-02-07 21:41:35 -0500

    Key prop added to FormSplitBill component do render it every time the selectedId change

--- src/App.js
+++ src/App.js
@@ -68,7 +68,11 @@ export default function App() {
         </Button>
       </div>
       {selectedFriend && (
-        <FormSplitBill friend={selectedFriend} onSplitBill={handleSplitBill} />
+        <FormSplitBill
+          friend={selectedFriend}
+          onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
+        />
       )}
     </div>
   );
